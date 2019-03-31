040b3be3d0c45ad64a8b0978e51c849b25063587
Zhiyu Long <zhiyulong@email.arizona.edu>
2019-03-31 14:56:38 -0700

    Cam&CharMove

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform man;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = new Vector3(man.position.x, transform.position.y, man.position.z-5);
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/chaMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaMove : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal") * speed;  //x
+        float vert = Input.GetAxis("Vertical") * speed;  //z
+
+        transform.position = new Vector3(transform.position.x + horiz, transform.position.y, transform.position.z + vert);
+
+    }
+}
