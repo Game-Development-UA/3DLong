6c0ded98a57b2bacb3554b3934473a1e9cb0cc17
Zhiyu Long <zhiyulong@email.arizona.edu>
2019-04-05 18:22:38 -0700

    camRotate

--- Assets/script/CameraMove.cs
+++ Assets/script/CameraMove.cs
@@ -10,10 +10,12 @@ public class CameraMove : MonoBehaviour
     public float dis;
     public float heightDis;
 
+
+    float initalRotateY;
     // Start is called before the first frame update
     void Start()
     {
-
+        initalRotateY = transform.rotation.y;
     }
 
     // Update is called once per frame
@@ -22,6 +24,13 @@ public class CameraMove : MonoBehaviour
 
         float height = man.position.y + heightDis;
         Vector3 target = new Vector3(man.position.x, height, man.position.z-dis);
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float rotateY = man.rotation.y - initalRotateY;
+            transform.Rotate(0, rotateY, 0);
+            initalRotateY = transform.rotation.y;
+        }
 
         transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
     }
