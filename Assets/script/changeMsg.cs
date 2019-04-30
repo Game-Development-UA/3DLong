fe6649a887f0071be5897b267d79f4cecd797d1e
Zhiyu Long <zhiyulong@email.arizona.edu>
2019-04-30 04:17:39 -0700

    fixed the problem of UI

--- Assets/script/changeMsg.cs
+++ Assets/script/changeMsg.cs
@@ -72,7 +72,7 @@ public class changeMsg : MonoBehaviour
     {
         if (giveMsg == 1)
         {
-            if (index < 4)
+            if (index < 5)
             {
                 int tempIndex = 0;
                 foreach (string temp in hints)
